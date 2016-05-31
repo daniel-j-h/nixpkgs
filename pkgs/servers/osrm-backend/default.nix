118d22596842fe305f5cedc5855580bcf216be35
Daniel J. Hofmann <daniel@trvx.org>
2016-05-31 20:52:38 +0200

    osrm-backend: 4.9.0 -> 5.2.0

--- pkgs/servers/osrm-backend/default.nix
+++ pkgs/servers/osrm-backend/default.nix
@@ -1,30 +1,30 @@
-{stdenv, fetchFromGitHub, cmake, luabind, libosmpbf, stxxl, tbb, boost, expat, protobuf, bzip2, zlib, substituteAll}:
+{stdenv, fetchFromGitHub, cmake, luabind, stxxl, tbb, boost, expat, bzip2, zlib, substituteAll}:
 
 stdenv.mkDerivation rec {
   name = "osrm-backend-${version}";
-  version = "4.9.1";
+  version = "5.2.0-rc.1";
 
   src = fetchFromGitHub {
     rev = "v${version}";
     owner  = "Project-OSRM";
     repo   = "osrm-backend";
-    sha256 = "1r4dwniwxgfppnb9asdh98w5qxqwkjhp9gc5fabmck0gk73cwkcc";
+    sha256 = "1v4x146z1vw6ihsfvl1gicpi6ykdzgvdzhpy5iysjgz5dix92s7n";
   };
 
   patches = [
-    ./4.5.0-gcc-binutils.patch
+    ./5.2.0-gcc-binutils.patch
     (substituteAll {
-      src = ./4.5.0-default-profile-path.template.patch;
+      src = ./5.2.0-default-profile-path.template.patch;
     })
   ];
 
-  buildInputs = [ cmake luabind libosmpbf stxxl tbb boost expat protobuf bzip2 zlib ];
+  buildInputs = [ cmake luabind stxxl tbb boost expat bzip2 zlib ];
 
   postInstall = "mkdir -p $out/share/osrm-backend && cp -r ../profiles $out/share/osrm-backend/profiles";
 
   meta = {
     homepage = https://github.com/Project-OSRM/osrm-backend/wiki;
-    description = "Open Source Routing Machine computes shortest paths in a graph. It was designed to run well with map data from the Openstreetmap Project";
+    description = "The Open Source Routing Machine is a high performance routing engine written in C++11 designed to run on OpenStreetMap data";
     license = stdenv.lib.licenses.bsd2;
   };
 }
